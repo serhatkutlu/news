eb3cd30855dfe4c1882a2b152ffdecc31b51cbc3
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2022-01-12 18:04:31 +0300

    navigation and navigation menu created

--- app/src/main/java/com/msk/news/ui/MainActivity.kt
+++ app/src/main/java/com/msk/news/ui/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.msk.news.ui
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.msk.news.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
+
 }
